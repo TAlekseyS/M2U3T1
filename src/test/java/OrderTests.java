86cceb2655a2f8ff5db29cfd9f0046a8682ae69f
Aleksey_T <talekseys@mail.ru>
2023-08-02 21:35:34 +0300

    menya poslaly

---
+++ src/test/java/OrderTests.java
@@ -0,0 +1,38 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class OrderTests {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+
+    @Test
+    void shouldTestSomething() throws InterruptedException {
+        driver.get("http://0.0.0.0:9999");
+        //   Thread.sleep(5000);
+
+    }
+
+}
--- src/test/java/Test.java
+++
@@ -1,9 +0,0 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
-
-public class Test {
-    @BeforeAll
-    public static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-}
